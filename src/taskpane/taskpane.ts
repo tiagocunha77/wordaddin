b3a7f7384fc2a3d060f8bf07082e2b60689963af
Tiago0709 <cunha37617@gmail.com>
2021-05-25 17:38:18 +0100

    final commit

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -9,6 +9,9 @@ import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
 
 /* global document, Office, Word */
+let json = (body: unknown, replacer?: (key: string, value: unknown) => unknown): string => {
+  return JSON.stringify(body !== undefined ? body : {}, replacer);
+};
 
 let makeRequest = (url: string, method: string = "GET", body?: BodyInit): Promise<any> => {
   let headers = new Headers();
@@ -22,6 +25,8 @@ let makeRequest = (url: string, method: string = "GET", body?: BodyInit): Promis
   if (token) {
     headers.append("Authorization", "Bearer " + token);
   }
+  console.log("teste43", url, method, headers, body);
+
   return fetch("https://tiagocunhapc:8443" + url, {
     method: method,
     headers: headers,
@@ -66,12 +71,20 @@ Office.onReady((info) => {
     document.getElementById("apply-style").onclick = applyStyle;
     document.getElementById("change-font").onclick = changeFont;
     document.getElementById("insert-text-into-range").onclick = insertTextIntoRange;
+    document.getElementById("insert-table").onclick = insertTable;
+    document.getElementById("insert-image").onclick = insertImage;
+    document.getElementById("send-comment").onclick = sendComment;
+    document.getElementById("select-box").onfocus = selectBox;
   }
 
   function login() {
     var data = new URLSearchParams();
-    data.append("username", "tiago");
-    data.append("password", "123456");
+
+    this.username = (document.getElementById("username") as HTMLInputElement).value;
+    this.password = (document.getElementById("password") as HTMLInputElement).value;
+    data.append("username", this.username);
+    data.append("password", this.password);
+    console.log("dadoslogin", this.username, this.password);
     makeRequest("/token", "POST", data).then((resp) => {
       try {
         sessionStorage.setItem("AuthToken", resp.jwt);
@@ -79,6 +92,102 @@ Office.onReady((info) => {
         console.info("token", resp);
         document.querySelector(".need-login").classList.remove("need-login");
       } catch {}
+    });
+  }
+  function selectBox() {
+    Word.run(function (context) {
+      return makeRequest("/desenhos").then((desenhos) => {
+        let selectbox = document.getElementById("select-box") as HTMLSelectElement;
+        document.querySelectorAll("#select-box option").forEach((option) => option.remove());
+        desenhos.forEach((desenho) => {
+          let opt = document.createElement("option");
+          opt.value = desenho.id;
+          opt.text = desenho.titulo;
+          selectbox.add(opt);
+        });
+
+        return context.sync();
+      });
+    }).catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+    });
+  }
+
+  function sendComment() {
+    Word.run(function (context) {
+      var selectedRange = context.document.getSelection();
+      context.load(selectedRange, "text");
+
+      return context.sync().then(function () {
+        console.log("rangess", selectedRange.text);
+
+        let comment = {
+          desenho: {
+            id: 1,
+          },
+          texto: selectedRange.text,
+        };
+
+        makeRequest("/comentario", "POST", json(comment));
+        return context.sync();
+      });
+    });
+  }
+
+  function insertImage() {
+    Word.run(function (context) {
+      return makeRequest("/desenhos").then((img) => {
+        img.forEach((desenhos) => {
+          let index = desenhos.desenho.indexOf("base64,");
+          let base64 = desenhos.desenho.substring(index + 7);
+
+          context.document.body.insertInlinePictureFromBase64(base64, "Start");
+        });
+        return context.sync();
+      });
+    }).catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+    });
+  }
+
+  function insertTable() {
+    Word.run(function (context) {
+      var body = context.document.body;
+
+      return makeRequest("/autor").then((tabs) => {
+        let tableData = [];
+        let titulos = ["Autor", "Titulo Desenho"];
+        tableData.push(titulos);
+        console.info("tabela", tabs);
+        tabs.forEach((autor) => {
+          autor.desenhos.forEach((desenho) => {
+            let linha = [autor.nome, desenho.titulo];
+            tableData.push(linha);
+          });
+        });
+        console.log("tabela 222", tableData);
+        body.insertTable(tableData.length, 2, "End", tableData);
+
+        return context.sync();
+      });
+      // var tableData = [
+      //   ["Nome", "Desenho"],
+      //   ["autor1", "desenho1"],
+      //   ["autor2", "desenho2"],
+      // ];
+
+      // secondParagraph.insertTable(3, 2, "After", tableData);
+    }).catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
     });
   }
 
@@ -90,6 +199,7 @@ Office.onReady((info) => {
         console.info("resp boy", resp);
         resp.forEach((autor) => {
           let p = docBody.insertParagraph(autor.nome, "Start");
+
           autor.desenhos.forEach((desenho) => {
             p.insertText("\n \t" + desenho.titulo, "End");
           });
